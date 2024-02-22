4c4c344cea525fb537892d8d759c3ee5ee4b4348
abdullahyalcin <apoyalcn98@gmail.com>
2024-02-22 14:01:47 +0100

    files

---
+++ tf-files/variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "your username"
+}
+
+variable "git-token" {
+  default = "your token"
+}
+
+variable "key-name" {
+  default = "your key pair"
+}
+
+variable "hosted-zone" {
+  default = "your hosted zone"
+}
